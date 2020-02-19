e0803eacdb62b2b27815a4b17d8259c39b8bced9
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2020-02-19 16:53:32 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-rm -rf /usr/share/tomcat/webapps/build/libs/*.jar*
+yum -y install unzip
+wget -P /opt https://services.gradle.org/distributions/gradle-6.2-all.zip
+sh /opt/gradle-6.2/bin/gradle wrapper
+sh /opt/gradle-6.2/bin/gradlew cleanAll loadDefault
