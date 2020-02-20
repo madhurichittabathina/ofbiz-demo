682694c7b8059184e5e2569572954530eb596792
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2020-02-20 14:53:06 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yum -y install unzip
 wget -P /opt https://services.gradle.org/distributions/gradle-6.2-all.zip
+unzip gradle-6.2-all.zip
 sh /opt/gradle-6.2/bin/gradle wrapper
 sh /opt/gradle-6.2/bin/gradlew cleanAll loadDefault
