a1c2152cb0885bb780f03fadeadb68575999755e
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2020-02-21 13:43:22 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+CWD=$(pwd)
 sudo yum -y install unzip
+echo $CWD
 sudo wget -P /opt https://services.gradle.org/distributions/gradle-6.2-all.zip
+echo $CWD
 sudo unzip /opt/gradle-6.2-all.zip
 sudo sh /opt/gradle-6.2/bin/gradle wrapper
 sudo mv gradlew /opt/gradle-6.2/bin
