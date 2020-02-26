1aeacc574d7830bc26461157e5a9471377d80c62
madhurichittabathina <54107842+madhurichittabathina@users.noreply.github.com>
2020-02-26 14:18:48 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 sudo cd /opt
 sudo git clone https://github.com/madhurichittabathina/ofbiz-demo.git
 sudo cd ofbiz-demo/
-./gradlew cleanAll loadDefault
-./gradlew ofbiz
+sudo ./gradlew cleanAll loadDefault
+sudo ./gradlew ofbiz
